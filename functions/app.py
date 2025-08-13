f0052879a596820e8f11155c162f701473504dcf
Marki <markimarkiando@gmail.com>
2025-08-13 13:25:12 -0600

    commets

--- functions/app.py
+++ functions/app.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template,redirect,request,url_for
 from callSamita import create_embedding, create_json_with_embeddings,find_closest_match, find_top_matches
 
 app = Flask(__name__)
-
+# inicio
 @app.route("/", methods=['GET'])
 def home():
     return render_template('index.html')
-
+#Aqui aparece el top 3 
 @app.route("/results", methods=['POST'])
+#   
 def showInfo():                                           #limpia el texto
     company_name = request.form.get("company_name")
     company_description = request.form.get("company_description")
@@ -19,6 +20,8 @@ def showInfo():                                           #limpia el texto
     #return render_template ('results.html', user_question=clase, descripcion= descripcion)
     results = find_top_matches(embedding)
     return render_template ('results.html', results=results)
+
+
 @app.route("/create_json", methods=['GET'])
 def createjson():                                           #limpia el texto
     create_json_with_embeddings()
@@ -28,10 +31,6 @@ def createjson():                                           #limpia el texto
 #def enviarform(request: datos):
     #response = callsamita(datos)3
     #return render_template('resultados.html')
-
-
-
-
 
 if __name__=='__main__':
     app.run(debug=True)
--- functions/callSamita.py
+++ functions/callSamita.py
@@ -7,51 +7,14 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 
 def create_embedding(question: str): #tipo str
-    """Retrieve the embedding for the given text using OpenAI API."""
+    """Convierte texto a embedding"""
     response = openai_client.embeddings.create(
         input=question,
         model="text-embedding-3-large" 
     )
     return response.data[0].embedding #envia un json
-
-def find_top_matches(question_embedding, top_n=3):
-    # Load once at startup
-    with open("results.json", "r", encoding="utf-8") as f:
-        RECORDS = json.load(f)
-
-    # Calculate similarity for each record
-    scored_records = []
-    for record in RECORDS:
-        similarity = cosine_similarity(
-            np.array(record["embedding"]).reshape(1, -1),
-            np.array(question_embedding).reshape(1, -1)
-        )[0][0]
-        scored_records.append((record, similarity))
-
-    # Sort by similarity descending
-    scored_records.sort(key=lambda x: x[1], reverse=True)
-
-    # Return the top N matches
-    return scored_records[:top_n]
-
-
-def find_closest_match(question_embedding):
-    best_score = -1
-    best_record = None
-
-    # Load once at startup
-    with open("results.json", "r", encoding="utf-8") as f:
-        RECORDS = json.load(f)
-    for record in RECORDS:
-        #similarity = cosine(question_embedding, record["embedding"])
-        similarity  = cosine_similarity(np.array(record["embedding"]).reshape(1, -1), np.array(question_embedding).reshape(1, -1))[0][0]
-        if similarity > best_score:
-            best_score = similarity
-            best_record = record
-
-    #return best_record, best_score
-    return best_record, best_score
-
+# Tenemos una lista de todas las clases de IMPI, las cuales convertimos en embedding
+# y guardamos los resultados en un archivo json
 def create_json_with_embeddings():
 # Ruta de entrada y salida
     csv_file = "data\clasificacion_niza_clase_descripcion_v3.csv"   # Tu archivo CSV con columnas: clase, descripcion
@@ -78,3 +41,43 @@ def create_json_with_embeddings():
         json.dump(resultados, f, ensure_ascii=False, indent=4)
 
     print(f"Procesado completo. {len(resultados)} registros guardados en {json_file}")
+#     Compara la descripcion de la marca del usuario,
+#     con los vectores del archivo json
+def find_top_matches(question_embedding, top_n=3):
+    # Lo carga una vez
+    with open("results.json", "r", encoding="utf-8") as f:
+        RECORDS = json.load(f)
+
+    # Calcula la similaridad de cada registro
+    scored_records = []
+    for record in RECORDS:
+        similarity = cosine_similarity(
+            np.array(record["embedding"]).reshape(1, -1),
+            np.array(question_embedding).reshape(1, -1)
+        )[0][0]
+        scored_records.append((record, similarity))
+
+    # Ordena la similaridad
+    scored_records.sort(key=lambda x: x[1], reverse=True)
+
+    # Regresa un top 3 de recomendaciones
+    return scored_records[:top_n]
+
+
+#def find_closest_match(question_embedding):
+   # best_score = -1
+   # best_record = None
+""" 
+    # Load once at startup
+    with open("results.json", "r", encoding="utf-8") as f:
+        RECORDS = json.load(f)
+    for record in RECORDS:
+        #similarity = cosine(question_embedding, record["embedding"])
+        similarity  = cosine_similarity(np.array(record["embedding"]).reshape(1, -1), np.array(question_embedding).reshape(1, -1))[0][0]
+        if similarity > best_score:
+            best_score = similarity
+            best_record = record
+
+    #return best_record, best_score
+    return best_record, best_score
+"""
